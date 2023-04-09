1d788cf640cfc5b3a48360e6607274517647c316
Jun <peng940825@gmail.com>
2023-04-09 20:05:27 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/temp/",
+  base: "/temp/dist",
   plugins: [vue()],
   resolve: {
     alias: {
