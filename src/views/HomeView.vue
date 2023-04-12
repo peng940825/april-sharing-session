42b7921ae5880faea911c97f3e7c42f312487083
Jun <peng940825@gmail.com>
2023-04-12 08:23:52 +0800

    u home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,33 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const innerWidth = ref(null);
+const innerHeight = ref(null);
+
+const outerWidth = ref(null);
+const outerHeight = ref(null);
+
+onMounted(() => {
+  innerWidth.value = window.innerWidth;
+  innerHeight.value = window.innerHeight;
+
+  outerWidth.value = window.outerWidth;
+  outerHeight.value = window.outerHeight;
+});
+</script>
+
 <template>
   <p>Home View</p>
   <p>Jun</p>
+  <p>{{ innerWidth }}, {{ innerHeight }}</p>
+  <p>{{ outerWidth }}, {{ outerHeight }}</p>
 </template>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display&display=swap");
 
 p {
-  font-size: 64px;
+  font-size: 48px;
   font-family: "Sedgwick Ave Display", cursive;
   text-align: center;
 }
