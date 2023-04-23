ee6ae5502d753009b504536519ba64352168c685
Jun <peng940825@gmail.com>
2023-04-23 08:57:02 +0800

    視頻資源換回本地

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -15,8 +15,9 @@ const options = {
   bigPlayButton: false,
   sources: [
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/juntify-fd26d.appspot.com/o/%E6%88%91%E7%AB%99%E5%9C%A8%E9%9B%B2%E6%9E%97.mp4?alt=media&token=a452977a-07f3-4440-8214-35942f751c54",
+      // src: "https://firebasestorage.googleapis.com/v0/b/juntify-fd26d.appspot.com/o/%E6%88%91%E7%AB%99%E5%9C%A8%E9%9B%B2%E6%9E%97.mp4?alt=media&token=a452977a-07f3-4440-8214-35942f751c54",
       type: "video/mp4",
+      src: "/src/assets/video.mp4",
     },
   ],
 };
